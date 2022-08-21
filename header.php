14971bed5de1a49699df4927a50202ea7059c476
mrxaria <111668334+mrxaria@users.noreply.github.com>
2022-08-21 06:14:06 +0430

    header.php
    this is header file

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" >
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+
+<script type="text/javascript" src="js/jquery.min.js" > </script>
+<script type="text/javascript" src="js/bootstrap.min.js" > </script>
+<script type="text/javascript" src="js/script.js" > </script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>صفحه اصلی</title>
+
+</head>
+
+<body>
+<div id="container" class="col-lg-12 col-md-12 col-sm-12 col-xs-12" >
+	
+    <div class="menue col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ 
+    </div>
+	<div  id="banner" class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div id="logo" class="col-lg-4 col-md-4 col-sm-4 col-xs-4" style="float:left;" >
+			<img src="images/hackpic.jpg" width="300" style="margin-top:-5px;">
+		</div>
+		
+		<div style="direction:ltr;" id="search" class="col-lg-4 col-md-hide col-sm-hide hidden-xs">
+				
+		</div>
+		
+		<div style="margin-top:0;float:right;" id="notif" class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+		<img src="images/fb.jpg" width="100" >
+		
+		</div>
+	
+	</div>
+	
+    <div id="content" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+     
+        
+       
